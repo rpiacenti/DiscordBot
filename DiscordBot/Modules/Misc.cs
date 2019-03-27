000c25f7b14750bf3b429caaaf404d0e65e60a66
ronaldiGames <ronald@igamesinc.com>
2019-03-27 15:28:09 -0600

    Initial commit Discord Bot

---
+++ DiscordBot/Modules/Misc.cs
@@ -0,0 +1,44 @@
+﻿using Discord;
+using Discord.Commands;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiscordBot.Modules
+{
+    public class Misc : ModuleBase<SocketCommandContext>
+    {
+        [Command("echo")]
+        public async Task Echo([Remainder]string message)
+        {
+            var embed = new EmbedBuilder();
+            embed.WithTitle("Message by " + Context.User.Username);
+            embed.WithDescription(message);
+            embed.WithColor(new Color(0, 255, 0));
+
+            await Context.Channel.SendMessageAsync("", false, embed);
+
+        }
+
+        [Command("pick")]
+        public async Task PickOne([Remainder]string message)
+        {
+            string[] options = message.Split(new char[] { '|' }, StringSplitOptions.RemoveEmptyEntries);
+
+            Random r = new Random();
+            string Selection = options[r.Next(0,options.Length)];
+
+            var embed = new EmbedBuilder();
+            embed.WithTitle("Choice for by " + Context.User.Username);
+            embed.WithDescription(Selection);
+            embed.WithColor(new Color(0, 255, 0));
+            embed.WithThumbnailUrl("https://www.flyingmag.com/sites/flyingmag.com/files/styles/655_1x_/public/import/embedded/sites/all/files/_images/201308/FLY0813_Disney_01.jpg?itok=oQeBXbIx");
+            //Context.User.GetAvatarUrl
+
+            await Context.Channel.SendMessageAsync("", false, embed);
+
+        }
+    }
+}
---
+++ DiscordBot/Utilities.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using System.IO;
+
+namespace DiscordBot
+{
+    class Utilities
+    {
+        private static Dictionary<String, String> alerts;
+
+        static Utilities()
+        {
+            string json = File.ReadAllText("SystemLang/alerts.json");
+            var data = JsonConvert.DeserializeObject<dynamic>(json);
+            alerts = data.ToObject< Dictionary<String, String>>();
+
+        }
+
+        public static string GetAlert(string key)
+        {
+            if (alerts.ContainsKey(key)) return alerts[key];
+            return "";
+        }
+
+    }
+}
